72e5b5119a0659060054e69dc119c65f0107184d
Arifkian <achmadrifkiansyori@gmail.com>
2021-07-10 19:48:09 +0700

    fill class UserPreference

--- app/src/main/java/com/fadtech/challengechap6kel1/preference/UserPreference.kt
+++ app/src/main/java/com/fadtech/challengechap6kel1/preference/UserPreference.kt
@@ -1,2 +1,27 @@
 package com.fadtech.challengechap6kel1.preference
 
+import android.content.Context
+import android.content.SharedPreferences
+
+class UserPreference(var context: Context) {
+
+    private var preference: SharedPreferences = context.getSharedPreferences(NAME, MODE)
+
+    companion object {
+        private const val NAME = "RockScissorPaper"
+        private const val MODE = Context.MODE_PRIVATE
+        private val PREF_USER_NAME = Pair("IS_USER_NAME", null)
+    }
+
+    var userName : String?
+        get() = preference.getString(PREF_USER_NAME.first, PREF_USER_NAME.second)
+        set(value) = preference.edit {
+            it.putString(PREF_USER_NAME.first, value)
+        }
+}
+
+private inline fun SharedPreferences.edit(operation: (SharedPreferences.Editor) -> Unit) {
+    val editor = edit()
+    operation(editor)
+    editor.apply()
+}
