574318df8a5d9717f626ad5c9c26039e3f8f1cac
Arifkian <achmadrifkiansyori@gmail.com>
2021-07-11 17:53:03 +0700

    design layout main_activity done

--- app/src/main/java/com/fadtech/challengechap6kel1/ui/main/MainActivity.kt
+++ app/src/main/java/com/fadtech/challengechap6kel1/ui/main/MainActivity.kt
@@ -3,10 +3,20 @@ package com.fadtech.challengechap6kel1.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fadtech.challengechap6kel1.R
+import com.fadtech.challengechap6kel1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var playMode: Int? = null
+    private var player2Choice: Int? = null
+    private var player1Choice: Int? = null
+    private var flag: Int = -1
+    private val TAG = MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
     }
 }
