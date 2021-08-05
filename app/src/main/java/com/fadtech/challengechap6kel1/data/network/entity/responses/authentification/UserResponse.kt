bb0102f6360668950d8fceca20e35b99c41174fe
Arifkian <achmadrifkiansyori@gmail.com>
2021-08-05 19:28:53 +0700

    add BaseAuthresponse , BinarApiService , SessionPreference, UserResponse

---
+++ app/src/main/java/com/fadtech/challengechap6kel1/data/network/entity/responses/authentification/UserResponse.kt
@@ -0,0 +1,12 @@
+package com.fadtech.challengechap6kel1.data.network.entity.responses.authentification
+
+import com.google.gson.annotations.SerializedName
+
+data class UserResponse(
+    @SerializedName("email")
+    val email: String? = null,
+    @SerializedName("_id")
+    val id: String? = null,
+    @SerializedName("username")
+    val username: String? = null
+)
---
+++ app/src/main/java/com/fadtech/challengechap6kel1/data/network/services/BinarApiServices.kt
@@ -0,0 +1,59 @@
+package com.fadtech.challengechap6kel1.data.network.services
+
+import com.fadtech.challengechap6kel1.BuildConfig
+import com.fadtech.challengechap6kel1.data.local.sharepreference.SessionPreference
+import com.fadtech.challengechap6kel1.data.network.entity.request.authentification.RegisterRequest
+import com.fadtech.challengechap6kel1.data.network.entity.responses.authentification.BaseAuthResponse
+import com.fadtech.challengechap6kel1.data.network.entity.responses.authentification.UserResponse
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import java.util.concurrent.TimeUnit
+
+interface BinarApiServices {
+
+
+    @POST("api/v1/auth/register")
+    suspend fun postRegisterData(@Body registerRequest: RegisterRequest) : BaseAuthResponse<UserResponse, String>
+
+    @GET("api/v1/auth/me")
+    suspend fun getSyncData() : BaseAuthResponse<UserResponse,String>
+
+
+    companion object{
+        private var retrofitServices : BinarApiServices? = null
+        fun getInstance(sessionPreference: SessionPreference) : BinarApiServices?{
+            if(retrofitServices == null){
+                //todo : add interceptor token
+                val authInterceptor = Interceptor{
+                    val requestBuilder = it.request().newBuilder()
+                    sessionPreference.authToken?.let { token ->
+                        requestBuilder.addHeader("Authorization", "Bearer $token")
+                    }
+                    it.proceed(requestBuilder.build())
+                }
+
+                //initialize
+                val okHttpClient = OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                    .addInterceptor(authInterceptor)
+                    .connectTimeout(120, TimeUnit.SECONDS)
+                    .readTimeout(120, TimeUnit.SECONDS)
+                    .build()
+
+                val retrofit = Retrofit.Builder()
+                    .baseUrl(BuildConfig.BASE_URL_BINAR_AUTH)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
+                    .build()
+                retrofitServices = retrofit.create(BinarApiServices::class.java)
+            }
+            return retrofitServices
+        }
+    }
+}
